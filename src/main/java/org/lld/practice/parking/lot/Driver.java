8e5286be7a2b5d5ae5507209496bbb3c330b9907
raviguruiitr@gmail.com <raviguruiitr@gmail.com>
2023-02-15 17:40:04 +0530

    Parking lot driver input program modified

--- src/main/java/org/lld/practice/parking/lot/Driver.java
+++ src/main/java/org/lld/practice/parking/lot/Driver.java
@@ -10,9 +10,19 @@ public class Driver {
         System.out.println(parkingLot.parkVehicle(new Car("Car_3")));
         System.out.println(parkingLot.parkVehicle(new Car("Car_4")));
         System.out.println(parkingLot.parkVehicle(new Car("Car_5")));
+        System.out.println(parkingLot.parkVehicle(new Car("Car_6")));
+        System.out.println(parkingLot.parkVehicle(new Car("Car_7")));
+        System.out.println(parkingLot.parkVehicle(new Car("Car_8")));
+        System.out.println(parkingLot.parkVehicle(new Car("Car_9")));
+        System.out.println(parkingLot.parkVehicle(new Car("Car_10")));
+        System.out.println(parkingLot.parkVehicle(new Car("Car_11")));
+        System.out.println(parkingLot.parkVehicle(new Car("Car_12")));
         System.out.println(parkingLot.parkVehicle(new Bus("Bus_1")));
+        System.out.println(parkingLot.parkVehicle(new Bus("Bus_3")));
+        parkingLot.print();
+        System.out.println("This must be Failed :" + parkingLot.parkVehicle(new Car("Car_13")));
         System.out.println(parkingLot.unParkVehicle("Car_5"));
-        System.out.println(parkingLot.parkVehicle(new Bus("Bus_1")));
+
         parkingLot.print();
         parkingLot.unParkVehicle("Car_3");
         parkingLot.unParkVehicle("Car_4");
